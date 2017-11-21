7cc88fcef9cbb7cb69c0929306f490b98c332efc
Daniel Kukieła <daniel@kukiela.pl>
2017-11-22 00:50:16 +0100

    Use settings

--- utils/pairing_testing_outputs.py
+++ utils/pairing_testing_outputs.py
@@ -1,19 +1,21 @@
-#quick file to pair epoch outputs w/ original test file 
+import sys
+sys.path.insert(0, '../')
+from setup.settings import hparams
+import os
 
-output_file_location = 'output_dev'
-tst_file_location = 'tst2013.from'
 
+# Quick file to pair epoch outputs w/ original test file
 if __name__ == '__main__':
-    with open(output_file_location,"r") as f:
+    with open(os.path.join(hparams['out_dir'], 'output_dev'), 'r') as f:
         content = f.read()
         to_data = content.split('\n')
 
-    with open(tst_file_location,"r") as f:
+    with open(hparams['dev_prefix'] + '.' + hparams['src'], 'r') as f:
         content = f.read()
         from_data = content.split('\n')
 
     for n, _ in enumerate(to_data[:-1]):
         print(30*'_')
-        print('>',from_data[n])
+        print('>', from_data[n])
         print()
-        print('Reply:',to_data[n])
+        print('Reply:', to_data[n])
