4577acd4049c3e38f57d0368583bcb0929832d10
Harrison <Harrison@sentdex.com>
2017-11-21 15:11:12 -0600

    Create pairing_testing_outputs.py
    Makes for a more read-able output_* file so you can get a better idea if responses are actually meaningful or not.

---
+++ pairing_testing_outputs.py
@@ -0,0 +1,19 @@
+#quick file to pair epoch outputs w/ original test file 
+
+output_file_location = 'output_dev-30000'
+tst_file_location = 'tst2013.from'
+
+if __name__ == '__main__':
+    with open(output_file_location,"r") as f:
+        content = f.read()
+        to_data = content.split('\n')
+
+    with open(tst_file_location,"r") as f:
+        content = f.read()
+        from_data = content.split('\n')
+
+    for n, _ in enumerate(to_data[:-1]):
+        print(30*'_')
+        print('>',from_data[n])
+        print()
+        print('Reply:',to_data[n])
