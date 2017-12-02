8e11169d5a1a637e0a2c5b1d72408a1d7c7a29a0
Daniel Kukieła <daniel@kukiela.pl>
2017-12-02 16:57:16 +0100

    Fixed prepare data issue

--- setup/prepare_data.py
+++ setup/prepare_data.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, '../')
 import os
 import errno
@@ -12,18 +13,18 @@ from multiprocessing import Pool
 from threading import Thread
 import time
 
-
 # Files to be prepared
 files = {
-    'train.from':   {'amount': 1,  'up_to': -1},  # copy all of data (up to "samples")
+    'train.from': {'amount': 1, 'up_to': -1},  # copy all of data (up to "samples")
     'tst2012.from': {'amount': .1, 'up_to': preprocessing['test_size']},  # copy 1/10th but up to 'test_size'
     'tst2013.from': {'amount': .1, 'up_to': preprocessing['test_size']},
-    'train.to':     {'amount': 1,  'up_to': -1},
-    'tst2012.to':   {'amount': .1, 'up_to': preprocessing['test_size']},
-    'tst2013.to':   {'amount': .1, 'up_to': preprocessing['test_size']},
+    'train.to': {'amount': 1, 'up_to': -1},
+    'tst2012.to': {'amount': .1, 'up_to': preprocessing['test_size']},
+    'tst2013.to': {'amount': .1, 'up_to': preprocessing['test_size']},
 }
 
 vocab = Counter([])
+
 
 # Prepare all files
 def prepare():
@@ -46,11 +47,13 @@ def prepare():
         print("\nFile: {} (iteration = 10k lines)".format(file_name))
 
         # Output file handler
-        out_file = open('{}/{}'.format(preprocessing['train_folder'], file_name), 'w', encoding='utf-8', buffering=131072)
+        out_file = open('{}/{}'.format(preprocessing['train_folder'], file_name), 'w', encoding='utf-8',
+                        buffering=131072)
 
         # Maximum number of lines
         read = 0
-        amount = int(min(amounts['amount'] * preprocessing['samples'] if preprocessing['samples'] > 0 else 10**20, amounts['up_to'] if amounts['up_to'] > 0 else 10**20))
+        amount = int(min(amounts['amount'] * preprocessing['samples'] if preprocessing['samples'] > 0 else 10 ** 20,
+                         amounts['up_to'] if amounts['up_to'] > 0 else 10 ** 20))
 
         # Prepare thread variables
         write_thread = None
@@ -61,7 +64,8 @@ def prepare():
         with Pool(processes=preprocessing['cpu_count']) as pool:
 
             # Open input file
-            with open('{}/{}'.format(preprocessing['source_folder'], file_name), 'r', encoding='utf-8', buffering=131072) as in_file:
+            with open('{}/{}'.format(preprocessing['source_folder'], file_name), 'r', encoding='utf-8',
+                      buffering=131072) as in_file:
 
                 # Iteraye every 19k lines
                 for rows in tqdm(read_lines(in_file, 10000, '')):
@@ -80,35 +84,37 @@ def prepare():
                     # zip_longest in read_lines adds extra empty lines up to batch size and we need to remove them
                     # but only for last batch - no need to do that for every batch
                     assert len(rows) == 10000
-                    read += 10000
+                    read += len(rows)
                     if read >= amount:
                         break
 
                     # We are going to process vocab in two threads - a bit faster than one and we need shared memory
                     # Also multiprocessing is slower here
-                    vocab_thread1 = Thread(target=append_vocab, args=(rows,1))
+                    vocab_thread1 = Thread(target=append_vocab, args=(rows, 1))
                     vocab_thread1.start()
-                    vocab_thread2 = Thread(target=append_vocab, args=(rows,2))
+                    vocab_thread2 = Thread(target=append_vocab, args=(rows, 2))
                     vocab_thread2.start()
 
                     # And thread for saving tokenized data to putput file
                     write_thread = Thread(target=write_lines, args=(out_file, rows))
                     write_thread.start()
 
+                    rows = []
+
         # Last vocab parts and last lines to write
-        vocab_thread1 = Thread(target=write_lines, args=(out_file, rows, True))
+        vocab_thread1 = Thread(target=append_vocab, args=(rows, 1))
         vocab_thread1.start()
-        vocab_thread2 = Thread(target=write_lines, args=(out_file, rows, True))
+        vocab_thread2 = Thread(target=append_vocab, args=(rows, 2))
         vocab_thread2.start()
-        write_thread = Thread(target=write_lines, args=(out_file, rows))
+        write_thread = Thread(target=write_lines, args=(out_file, rows, True))
         write_thread.start()
         vocab_thread1.join()
         vocab_thread2.join()
         write_thread.join()
+        out_file.close()
 
         # If it's train file, make vocab
         if file_name == 'train.from' or file_name == 'train.to':
-
             print("\nFile: {} (saving vocab)".format(file_name.replace('train', 'vocab')))
 
             # Get most common entities
@@ -122,24 +128,28 @@ def prepare():
             vocab = [entity for entity in new_vocab if not (entity in vocab or vocab.add(entity))]
 
             # Write entities to a file
-            with open('{}/{}'.format(preprocessing['train_folder'], file_name.replace('train', 'vocab')), 'w', encoding='utf-8', buffering=131072) as vocab_file:
+            with open('{}/{}'.format(preprocessing['train_folder'], file_name.replace('train', 'vocab')), 'w',
+                      encoding='utf-8', buffering=131072) as vocab_file:
                 vocab_file.write("<unk>\n<s>\n</s>\n" + "\n".join(vocab[:preprocessing['vocab_size']]))
-            with open('{}/{}'.format(preprocessing['train_folder'], file_name.replace('train', 'vocab_unused')), 'w', encoding='utf-8', buffering=131072) as vocab_file:
+            with open('{}/{}'.format(preprocessing['train_folder'], file_name.replace('train', 'vocab_unused')), 'w',
+                      encoding='utf-8', buffering=131072) as vocab_file:
                 vocab_file.write("\n".join(vocab[preprocessing['vocab_size']:]))
+
 
 # Helper function, reads 'amount' number of lines from file handler
 def read_lines(file, amount, fillvalue=None):
     args = [iter(file)] * amount
     return zip_longest(*args, fillvalue=fillvalue)
 
-# Writle batch of lines to a file
-def write_lines(file, lines, last = False):
 
+# Writle batch of lines to a file
+def write_lines(file, lines, last=False):
     # Handling empty lines (described above)
     if last:
         lines = filter(None, list(lines))
 
-    file.write('\n'.join(lines) + '\n')
+    file.write('\n'.join(lines) + ('' if last else '\n'))
+
 
 # Append tokens to vocab
 def append_vocab(lines, thread):
@@ -159,9 +169,7 @@ def append_vocab(lines, thread):
     # Add entities to vocab
     vocab.update(local_vocab)
 
+
 # Prepare training data set
 if __name__ == "__main__":
     prepare()
-
-
-
