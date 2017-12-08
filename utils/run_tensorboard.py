3c972f3739783540f061dee6adfd33a61342e28e
Daniel Kukieła <daniel@kukiela.pl>
2017-12-08 02:24:59 +0100

    Fixed run issues on some OSes
    Fixes https://github.com/daniel-kukiela/nmt-chatbot/issues/10

--- utils/run_tensorboard.py
+++ utils/run_tensorboard.py
@@ -4,7 +4,7 @@ from setup.settings import hparams
 import subprocess
 
 
-p = subprocess.Popen('tensorboard --port 22222 --logdir {}'.format(hparams['out_dir']), stdout=subprocess.PIPE, bufsize=1)
+p = subprocess.Popen('tensorboard --port 22222 --logdir {}'.format(hparams['out_dir']), stdout=subprocess.PIPE, shell=True, bufsize=1)
 for line in iter(p.stdout.readline, b''):
     print(line),
 p.stdout.close()
